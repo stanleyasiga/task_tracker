bfc4e3b2a560ca436471573b53dac9247c130db0
Stanley <austinstan24@gmail.com>
2025-03-28 23:23:12 +0300

    added items through comandline

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 
-const readline = require("node:readline");
+// const readline = require("node:readline");
 
 const args = process.argv.slice(2);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+let count = 0;
 
 if (args.length === 0) {
-  console.log("Usage: mycli <your_name>");
+  console.log("Usage: task-cli <your_name>");
+} else if (args[0] === "add") {
+  let temp = count + 1;
+  console.log(`Task added successfully (ID: ${temp})`);
+  count = temp;
 } else {
   console.log(`Hello, ${args[0]}!`);
 }
 
-rl.question("What is your name? ", (name) => {
-  console.log(`Hello ${name}`);
-  rl.close();
-});
+//temp = 0 1
+//count = 0 1
+
+// rl.question("What is your name?", (name) => {
+//   console.log(`Hello ${name}`);
+//   rl.close();
+// });
