c70a03d15d90a7508310613251fc38fc60769c5b
Stanley <austinstan24@gmail.com>
2025-04-02 22:31:43 +0300

    adding to a file using json file symchronouly

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // const readline = require("node:readline");
 
 const args = process.argv.slice(2);
+const fs = require("fs");
 
 // const rl = readline.createInterface({
 //   input: process.stdin,
@@ -13,9 +14,42 @@ let count = 0;
 if (args.length === 0) {
   console.log("Usage: task-cli <your_name>");
 } else if (args[0] === "add") {
-  let temp = count + 1;
-  console.log(`Task added successfully (ID: ${temp})`);
-  count = temp;
+  let description = args[1];
+  let task = {
+    id: Date.now(),
+    description,
+    status: "todo",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  fs.readFile("task-cli.json", "utf-8", (err, data) => {
+    let jsonArray = [];
+
+    if (!err && data) {
+      try {
+        jsonArray = JSON.parse(data);
+        if (!Array.isArray(jsonArray)) return (jsonArray = []);
+      } catch (parseErr) {
+        console.error("Invalid JSON file:", parseErr);
+        return;
+      }
+    }
+    jsonArray.push(task);
+
+    fs.writeFile(
+      "task-cli.json",
+      JSON.stringify(jsonArray, null, 2),
+      "utf8",
+      (writeErr) => {
+        if (writeErr) {
+          console.error("Error writing to file:", writeErr);
+          return;
+        }
+        console.log(`Task added successfully (ID:${Date.now()})`);
+      }
+    );
+  });
 } else {
   console.log(`Hello, ${args[0]}!`);
 }
